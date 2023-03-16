c85b60c308d6de5e9c2f52b79709fee53632f4b5
Hydra <limonsito.kls@hotmail.com>
2023-03-16 00:06:12 -0600

    Prueba de overrideoverload

---
+++ Animal/src/com/example/Animal.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Animal {
+	public int NumberLegs = 4;
+
+	
+    public int getNumberLegs() {
+		return NumberLegs;
+	}
+
+	public void setNumberLegs(int numberLegs) {
+		NumberLegs = numberLegs;
+	}
+ 
+}
---
+++ Animal/src/com/example/Octopus.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class Octopus extends Animal{
+	public int NumberLegs = 8;
+
+	public int getNumberLegs() {
+		return NumberLegs;
+	}
+
+	public void setNumberLegs(int numberLegs) {
+		NumberLegs = numberLegs;
+	}
+
+
+
+	
+}
